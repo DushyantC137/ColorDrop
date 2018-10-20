0078817075419ed1e249c4f2322d68834e860215
DushChnt <32354434+DushChnt@users.noreply.github.com>
2018-10-20 16:51:02 +0530

    PlayerControlAndVillianControl
    Two scripts added mainly to control and destroy player
    +rotating villian objects script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public string thisColor;
+    public GameObject player;
+    Rigidbody2D rb;
+    public bool GameOver;
+    public SpriteRenderer sr;
+    public Color yellow;
+    public Color blue;
+    public Color red;
+    public Color pink;
+    public GameObject locationObject;
+    Vector2 locationVector; 
+    // Use this for initialization
+    
+    void Start()
+    {
+        
+        rb= player.GetComponent<Rigidbody2D>();
+        //sr = player.GetComponent<SpriteRenderer>();
+       thisColor= SetRandomColor();
+      //  Debug.Log("RANDOM " + thisColor);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        locationVector = locationObject.transform.position;
+        if (!GameOver)
+        {
+            if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
+            {
+               // Debug.Log("click");
+                rb.gravityScale = 2f;
+               // Debug.Log("thisColor=" + this.thisColor);
+            }
+
+        }
+    }
+    
+    void OnTriggerEnter2D(Collider2D col)
+    {
+     //RightColor
+        if (col.tag==this.thisColor)
+        {
+            Debug.Log("coltag " + col.tag + " thiscolor "+thisColor);
+            DestroyVillian();
+            StopPlayer();
+
+            
+        }
+        else if (col.tag !=this.thisColor)
+        {
+     //Wrong Color
+            Debug.Log("GameOver");
+            Destroy(player);
+        }
+     //Color Switch
+
+
+    }
+    string SetRandomColor()
+    { 
+        int index = Random.Range(0, 3);
+        switch (index)
+        {
+            case 0:
+                //      thisColor = "yellow";
+                sr.color = yellow;
+                return "yellow";
+                break;
+            case 1:
+                //thisColor = "pink";
+                sr.color = pink; Debug.Log("thisColor== " + thisColor);
+                return "pink";
+                break;
+            case 2:
+                //thisColor = "blue";
+                sr.color = blue;
+                return "blue";
+                break;
+            case 3:
+                //thisColor = "red";
+                sr.color = red;
+                return "red";
+                break;
+            default:
+                return "red";
+        }
+    }
+    public void DestroyVillian()
+    {
+        Debug.Log("Destroyed");
+        Destroy(GameObject.FindWithTag("villian"));
+    }
+    
+    public void StopPlayer()
+    {
+        rb.gravityScale = 0;
+        rb.velocity = new Vector2(0,0);
+        player.transform.position = locationVector;
+    }
+}
