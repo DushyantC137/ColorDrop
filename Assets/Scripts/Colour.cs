90d485ba47fa6213958850ce7f4603c52ea7df72
justFaGit <33806014+justFaGit@users.noreply.github.com>
2018-10-20 10:34:46 +0530

    Adding the assets folder for the Player Ball
    Contains the particle effect and random color generation

---
+++ Assets/Scripts/Colour.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyNameSpace{ public class Colour : MonoBehaviour
+        {
+            public Rigidbody2D rb;
+            public int currentColourCode;
+            public SpriteRenderer sp;
+            public Color ColourRed;
+            public Color ColourYellow;
+            public Color ColourPink;
+            public Color ColourBlue;
+            // Use this for initialization
+            void Start()
+            {
+       
+                SetRandomColour();
+       
+            }
+
+            // Update is called once per frame
+            void Update()
+            {
+       
+            }
+            public int GetCurrentColour()
+            {
+                return currentColourCode;
+            }
+   
+            void SetRandomColour()
+            {
+                int index = Random.Range(0, 3);
+                switch (index)
+                {
+                    case 0:
+                        currentColourCode = 0; //Red
+                        sp.color = ColourRed;
+                        break;
+                    case 1:
+                        currentColourCode = 1; //Yellow
+                        sp.color = ColourYellow;
+                        break;
+                    case 2:
+                        currentColourCode = 2; //Pink
+                        sp.color = ColourPink;
+                        break;
+                    case 3:
+                        currentColourCode = 3; //Blue
+                        sp.color = ColourBlue;
+                        break;
+
+
+                }
+            }
+        }
+}
---
+++ Assets/Scripts/ParticleColour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+ namespace MyNameSpace
+{ public class ParticleColour : MonoBehaviour {
+
+        public Colour ballcolour;
+        public int value;
+        public ParticleSystem ps;
+        public Color startColor;
+        //public SpriteRenderer rd;
+        // Use this for initialization
+        void Start () {
+            var main = ps.main;
+            value = ballcolour.GetCurrentColour(); //gets colour of the ball
+            switch (value)
+            {
+                case 0:
+                   main.startColor = new Color(1, 0, 0, 1); //red
+                    //rd.color = startColour;
+                    break;
+                case 1:
+                    main.startColor = new Color(0.9f, 0.8f,0.04f,1f); //yellow
+                   // rd.color = startColour;
+                    break;
+                case 2:
+                    main.startColor = new Color(0.9f, 0.04f, 0.6f, 1f); //pink
+                    //rd.color = startColour;
+                    break;
+                case 3:
+                    main.startColor = new Color(0,0,1,1); //blue
+                    break;
+
+            }
+	    }
+
+	
+	    // Update is called once per frame
+	    void Update () {
+		
+	    }
+    }
+}
